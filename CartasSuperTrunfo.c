bfb1535ad59f0676597b3222f7ac251c3f8c4186
Dev Pereira <47191733+DevPereira1st@users.noreply.github.com>
2025-07-24 23:05:45 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,20 +2,20 @@
 
 int main() {
 
-    char estado[2], estado2[2];
-    char codigo[50], codigo2[50];
+    unsigned char estado[2], estado2[2];
+    unsigned char codigo[50], codigo2[50];
     char cidade[50], cidade2[50];
-    int populacao, populacao2;
+    unsigned int populacao, populacao2;
     float area, area2;
     float pib, pib2; 
-    int ptr, ptr2; /*pontos turisticos*/
+    unsigned int ptr, ptr2; /*pontos turisticos*/
     
     printf("𖨠 Bem Vindo ao Super Trunfo - Card City 𖨠\n\n ✢ Insira as informações solicitadas. ✢\n\n");
 
     // Cadastro de Carta Nº1
     printf("➢ Cadastre a primeira carta:\n\n");
 
-    printf("↠ Selecione uma letra para a carta, (Ex. A):");
+    printf("↠ Selecione uma letra para a carta, (Ex. A): ");
     scanf("%s",estado);
     printf("↠ Digite o codigo da Cidade: ");
     scanf("%s",codigo);
@@ -32,10 +32,12 @@ int main() {
 
     printf("✓ Carta Nº1 Cadastrada!\n\n");
 
-    float media, media3;
+    float media, media3, media6;
 
-    media = (float) (populacao + area) / 2;
-    media3 = (float) (pib + populacao) / 2;
+    media = (float) (populacao / area); /*Densidade populacional*/
+    media3 = (float) (pib / populacao); /*PIB Per Capita*/
+    media6 = (float) (area / populacao); /*Inverso Densidade populacional*/
+
 
     //Cadastro da Segunda Carta
 
@@ -58,10 +60,12 @@ int main() {
 
     printf("✓ Carta Nº2 Cadastrada!\n\n");
 
-    float media2, media4;
+    float media2, media4, media5;
 
-    media2 = (float) (populacao2 + area2) / 2;
-    media4 = (float) (pib2 + populacao2) / 2;
+    media2 = (float) (populacao2 / area2); /*Densidade populacional*/
+    media4 = (float) (pib2 / populacao2); /*PIB Per Capita*/
+    media5 = (float) (area2 / populacao2); /*Inverso Densidade populacional*/
+
 
     // Chat Log da Carta1
 
@@ -79,6 +83,17 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", media);
     printf("PIB Per Capita: %.2f reais\n\n", media3);
 
+    float desidadeepib; /*Puxar Densidade e PIB Per Capita*/
+    desidadeepib = (float) (media + media3);
+
+    printf("Valor de Densidade e PIB Per Capita: %.0f\n\n", desidadeepib);
+
+    float superpoder;
+    superpoder = populacao + area + pib + ptr + media3 + media6;
+
+    printf("Super Poder: %f\n\n", superpoder);
+
+
     // Chat log da Carta 2
 
     printf("┌┌Informações da Carta Nº2:┐┐\n\n");
@@ -92,6 +107,43 @@ int main() {
     printf("Total pontos turísticos: %d\n",ptr2);
     printf("Densidade Populacional: %.2f hab/km²\n", media2);
     printf("PIB Per Capita: %.2f reais \n\n", media4);
+
+    float desidadeepib2; /*Puxar Densidade e PIB Per Capita 2*/
+    desidadeepib2 = (float) (media2 + media4);
+
+    printf("Valor de Densidade e PIB Per Capita: %.0f\n\n", desidadeepib2);
+
+    float superpoder2;
+    superpoder2 = populacao2 + area2 + pib2 + ptr2 + media3 + media5;
+
+    printf("Super Poder: %f\n\n", superpoder2);
+
+    // Comparação das Cartas - Resultado /  Exceto estado, código e nome
+    // Populacao, area, pib, ptr, media, media3, superpoder
+    // Populacao2, area2, pib2, ptr2, media2, media4, superpoder2
+
+    int rpopulacao, rarea, rpib, rptr, rmedia, rmedia3, rsuperpoder;
+
+    rpopulacao = populacao > populacao2;
+    rarea = area > area2;
+    rpib = pib > pib2;
+    rptr = ptr > ptr2;
+    rmedia = media < media2;
+    rmedia3 = media3 > media4;
+    rsuperpoder = superpoder > superpoder2;
+    
+
+    printf("✦✦ Comparação das Cartas ✦✦\n\n");
+    printf("✦✦ SE O RESULTADO FOR 1, CARTA 1 VENCE ✦✦\n ✦✦ SE O RESULTADO FOR 0, CARTA 2 VENCE ✦✦\n\n");
+
+    printf("Resultado: %d\n\n", rarea);
+    printf("Resultado: %d\n\n", rpopulacao);
+    printf("Resultado: %d\n\n", rpib);
+    printf("Resultado: %d\n\n", rptr);
+    printf("Resultado: %d\n\n", rmedia);
+    printf("Resultado: %d\n\n", rmedia3);
+    printf("Resultado: %d\n\n", rsuperpoder);
+
 
     return 0;
 }
